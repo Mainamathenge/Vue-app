5518f4f8de224ae5e7ea2aaab4e86640a7e22dc7
Mainamathenge <mainamathengej@gmail.com>
2023-09-25 15:00:44 +0300

    loginpage

--- src/components/SignupLogin.vue
+++ src/components/SignupLogin.vue
@@ -1,11 +1,56 @@
 <template>
-  <v-sheet width="300" class="mx-auto">
-    <v-form ref="form">
+  <v-sheet
+    width="300"
+    class="mx-auto"
+    image="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"
+    permanent
+  >
+    <v-card class="custom-card"> Welcome To CMU DATA collection </v-card>
+    <v-form
+      ref="form"
+      image="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"
+      permanent
+    >
       <v-text-field
-        v-model="name"
+        v-model="FullName"
+        :rules="nameRules"
+        label="FullName"
+        required
+      ></v-text-field>
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="PhoneNumber"
         :counter="10"
         :rules="nameRules"
-        label="Name"
+        label="phone"
+        required
+      ></v-text-field>
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="email"
+        :rules="nameRules"
+        label="email"
+        required
+      ></v-text-field>
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="PhoneName"
+        :rules="nameRules"
+        label="PhoneName&Model"
+        required
+      ></v-text-field>
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="YearofManufacture"
+        :rules="nameRules"
+        label="Year of Manufacture"
+        required
+      ></v-text-field>
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="cameraSpecs"
+        :rules="nameRules"
+        label="CameraMegapicells"
         required
       ></v-text-field>
 
@@ -20,22 +65,22 @@
       <v-checkbox
         v-model="checkbox"
         :rules="[(v) => !!v || 'You must agree to continue!']"
-        label="Do you agree?"
+        label="Do you agree to the terms and conditions?"
         required
       ></v-checkbox>
 
       <div class="d-flex flex-column">
         <v-btn color="success" class="mt-4" block @click="validate">
-          Validate
+          SUBMIT
         </v-btn>
 
-        <v-btn color="error" class="mt-4" block @click="reset">
+        <!-- <v-btn color="error" class="mt-4" block @click="reset">
           Reset Form
         </v-btn>
 
         <v-btn color="warning" class="mt-4" block @click="resetValidation">
           Reset Validation
-        </v-btn>
+        </v-btn> -->
       </div>
     </v-form>
   </v-sheet>
@@ -46,3 +91,12 @@ export default {
   name: "HelloWorld",
 };
 </script>
+
+<style>
+.custom-card {
+  background-image: url(~@/assets/prostheticsImages/loginSignup.png);
+  background-size: cover;
+  backdrop-filter: blur(100px);
+  height: 150px;
+}
+</style>
