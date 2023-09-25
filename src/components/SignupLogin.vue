7099793dd94a9c284765e862a20285d24c024669
Mainamathenge <mainamathengej@gmail.com>
2023-09-25 15:24:13 +0300

    added-space

--- src/components/SignupLogin.vue
+++ src/components/SignupLogin.vue
@@ -1,93 +1,81 @@
 <template>
-  <v-sheet
-    width="300"
-    class="mx-auto"
-    image="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"
-    permanent
-  >
-    <v-card class="custom-card">
-      <div class="d-flex align-center justify-center vertical-center">
-        Welcome To CMU DATA collection
-      </div>
-    </v-card>
-    <v-form
-      ref="form"
+  <div class="scrollable-container">
+    <v-sheet
+      width="300"
+      class="mx-auto"
       image="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"
       permanent
     >
-      <v-text-field
-        v-model="FullName"
-        :rules="nameRules"
-        label="FullName"
-        required
-      ></v-text-field>
-      <v-spacer></v-spacer>
-      <v-text-field
-        v-model="PhoneNumber"
-        :counter="10"
-        :rules="nameRules"
-        label="phone"
-        required
-      ></v-text-field>
-      <v-spacer></v-spacer>
-      <v-text-field
-        v-model="email"
-        :rules="nameRules"
-        label="email"
-        required
-      ></v-text-field>
-      <v-spacer></v-spacer>
-      <v-text-field
-        v-model="PhoneName"
-        :rules="nameRules"
-        label="PhoneName&Model"
-        required
-      ></v-text-field>
-      <v-spacer></v-spacer>
-      <v-text-field
-        v-model="YearofManufacture"
-        :rules="nameRules"
-        label="Year of Manufacture"
-        required
-      ></v-text-field>
-      <v-spacer></v-spacer>
-      <v-text-field
-        v-model="cameraSpecs"
-        :rules="nameRules"
-        label="CameraMegapicells"
-        required
-      ></v-text-field>
+      <v-card class="custom-card">
+        <div class="d-flex align-center justify-center vertical-center">
+          Welcome To CMU DATA collection
+        </div>
+      </v-card>
+      <v-form
+        ref="form"
+        image="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"
+        permanent
+      >
+        <v-text-field
+          v-model="FullName"
+          :rules="nameRules"
+          label="FullName"
+          required
+        ></v-text-field>
+        <v-spacer></v-spacer>
+        <v-text-field
+          v-model="PhoneNumber"
+          :counter="10"
+          :rules="nameRules"
+          label="phone"
+          required
+        ></v-text-field>
+        <v-spacer></v-spacer>
+        <v-text-field
+          v-model="email"
+          :rules="nameRules"
+          label="email"
+          required
+        ></v-text-field>
+        <v-spacer></v-spacer>
+        <v-text-field
+          v-model="PhoneName"
+          :rules="nameRules"
+          label="PhoneName&Model"
+          required
+        ></v-text-field>
+        <v-spacer></v-spacer>
+        <v-text-field
+          v-model="YearofManufacture"
+          :rules="nameRules"
+          label="Year of Manufacture"
+          required
+        ></v-text-field>
+        <v-spacer></v-spacer>
+        <v-text-field
+          v-model="cameraSpecs"
+          :rules="nameRules"
+          label="CameraMegapicells"
+          required
+        ></v-text-field>
 
-      <!-- <v-select
-        v-model="select"
-        :items="items"
-        :rules="[(v) => !!v || 'Item is required']"
-        label="Item"
-        required
-      ></v-select> -->
+        <v-checkbox
+          v-model="checkbox"
+          :rules="[(v) => !!v || 'You must agree to continue!']"
+          label="Do you agree to the terms and conditions?"
+          required
+        ></v-checkbox>
 
-      <v-checkbox
-        v-model="checkbox"
-        :rules="[(v) => !!v || 'You must agree to continue!']"
-        label="Do you agree to the terms and conditions?"
-        required
-      ></v-checkbox>
-
-      <div class="d-flex flex-column">
-        <v-btn color="success" class="mt-4" block @click="validate">
-          SUBMIT
-        </v-btn>
-
-        <!-- <v-btn color="error" class="mt-4" block @click="reset">
-          Reset Form
-        </v-btn>
-
-        <v-btn color="warning" class="mt-4" block @click="resetValidation">
-          Reset Validation
-        </v-btn> -->
-      </div>
-    </v-form>
-  </v-sheet>
+        <div class="d-flex flex-column">
+          <v-btn color="success" class="mt-4" block @click="validate">
+            SUBMIT
+          </v-btn>
+          <div style="margin-bottom: 20px"></div>
+        </div>
+        <v-spacer></v-spacer>
+      </v-form>
+    </v-sheet>
+  </div>
 </template>
 
 <script>
@@ -97,6 +85,10 @@ export default {
 </script>
 
 <style>
+.scrollable-container {
+  max-height: 650px;
+  overflow-y: auto;
+}
 .custom-card {
   background-image: url(~@/assets/prostheticsImages/loginSignup.png);
   background-size: cover;
